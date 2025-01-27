45ed40c68e294253a4e3a1e519cf113d7bcdde3c
BMFASilva <bmfas.2002@gmail.com>
2025-01-27 14:19:55 +0000

    newTypeDefs

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,40 +1,66 @@
 import { gql } from "graphql-tag";
 
 export const typeDefs = gql`
-  type User {
-    id: ID!
-    username: String!  # Apenas retorna o nome do utilizador (a password nunca é devolvida)
-  }
+  # Definindo os tipos para as entidades da aplicação
 
-  type Post {
-    id: ID!
-    title: String!
-    content: String!
-    author: User! # Referência ao autor
-  }
+type User {
+  id: ID!
+  nome: String!
+  senha: String!
+  dataRegisto: String!
+}
 
-  # paginação por cursores
-  type PostPageInfo {
-    posts: [Post]
-    prevCursor: ID
-    nextCursor: ID
-  }
+type Gestacao {
+  id: ID!
+  usuarioId: ID!
+  dataInicio: String!
+  duracaoEstimativa: Int!
+  dataTerminoPrevisto: String!
+}
 
-  type Query {
-    me: User
-    users: [User]
-    posts(limit: Int, offset: Int): [Post]
-    posts_byCursor(limit: Int, cursor: ID): PostPageInfo
-  }
+type GestacaoRegistro {
+  id: ID!
+  gestacaoId: ID!
+  semana: Int!
+  peso: Float!
+  comprimento: Float!
+  dataRegistro: String!
+}
 
-  type Mutation {
-    register(username: String!, password: String!): User
-    login(username: String!, password: String!): String!
-    createPost(title: String!, content: String!): Post
-  }
+# Definindo as operações (queries e mutations)
 
-  # Subscrições para novos posts
-  type Subscription {
-    novoPostAdicionado: Post
-  }
+# Consultas (queries)
+
+type Query {
+  users: [User]
+  user(id: ID!): User
+  gestaos: [Gestacao]
+  gestacao(id: ID!): Gestacao
+  gestacaoRegistros: [GestacaoRegistro]
+  gestacaoRegistro(id: ID!): GestacaoRegistro
+}
+
+# Mutations (operações de escrita)
+
+type Mutation {
+  # Criar um novo usuário
+  createUser(nome: String!, senha: String!): User
+
+  # Atualizar informações do usuário
+  updateUser(id: ID!, nome: String, senha: String): User
+
+  # Criar uma nova gestação
+  createGestacao(usuarioId: ID!, dataInicio: String!, duracaoEstimativa: Int!): Gestacao
+
+  updateGestacao(id: ID!, dataInicio: String, duracaoEstimativa: Int): Gestacao
+
+  createGestacaoRegistro(gestacaoId: ID!, semana: Int!, peso: Float!, comprimento: Float!): GestacaoRegistro
+
+  updateGestacaoRegistro(id: ID!, semana: Int, peso: Float, comprimento: Float): GestacaoRegistro
+}
+
+type Subscription {
+  gestacaoCriada: Gestacao
+}
+
 `;
